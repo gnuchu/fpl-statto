bb6184bd3fe7723c8f3f25115019a8ef7a32370c
John Griffiths <gnuchu@gmail.com>
2020-10-01 14:51:59 +0100

    Add mutile tabs for different sorts

--- statto.py
+++ statto.py
@@ -2,18 +2,21 @@ import pandas as pd
 import numpy as np
 import requests
 from datetime import datetime
+import json
 
-def create_li_item(title, active):
+def create_li_item(title, active, sort_item):
+  tab_id = title.lower() + sort_item.lower()
   html = f'''
 <li class="nav-item">
-    <a href="#{title.lower()}" class="nav-link {active}" data-toggle="tab">{title}</a>
+    <a href="#{tab_id}" class="nav-link {active}" data-toggle="tab">{title}</a>
 </li>
 '''
   return html
 
-def create_tab_content(title, content, active):
+def create_tab_content(title, content, active, sort_item):
+  tab_id = title.lower() + sort_item.lower()
   html = f'''
-<div class="tab-pane fade show {active}" id="{title.lower()}">
+<div class="tab-pane fade show {active}" id="{tab_id}">
     <p>
       {content}
     </p>
@@ -28,16 +31,24 @@ def read_file(path):
   
   return data
 
+def write_file(path, content):
+  f = open(path, "w")
+  f.write(content)
+  f.close
+
+
 def process_dataframe(df, title):
   html = ''
   for index, row in df.iterrows():
     html += "<tr>"
-    html += "<td>" + str(row['second_name'])   + "</td>"
+    html += "<td>" + str(row['first_name']) + " " + str(row['second_name'])   + "</td>"
     html += "<td>" + str(row['team'])          + "</td>"
     #html += "<td>" + str(row['element_type'])  + "</td>"
     html += "<td>" + str(row['now_cost']/10)      + "</td>"
     html += "<td>" + str(row['value'])         + "</td>"
     html += "<td>" + str(row['total_points'])  + "</td>"
+    html += "<td>" + str(row['points_per_game'])  + "</td>"
+    html += "<td>" + str(row['minutes'][0])  + "</td>"
     html += "</tr>"
   
   return html
@@ -46,81 +57,83 @@ def date_str():
   return datetime.now().strftime('%A %d %B %Y %H:%M:%S')
 
 def date_line():
-  #html = '<hr/>'
   html = "<small>Page generated: " + date_str() + "</small>"
   html += '<hr/>'
   return html
 
-def table_maker(dataframe, title):
+def table_maker(dataframe, title, sort_item):
+  table_name = sort_item.lower() + "_table"
   html = f"<h4>{title}</h4><br/>" 
-  html += table_head()
+  html += table_head(table_name)
   html += process_dataframe(dataframe, title)
   html += table_foot()
   return html
 
-def table_head():
-  return read_file('templates/thead.html')
+def table_head(table_name):
+  table = read_file('templates/thead.html')
+  table = table.replace('table_name_place_holder', table_name)
+  return table
   
 def table_foot():
   return read_file('templates/tend.html')
 
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 r = requests.get(url)
-json = r.json()
+response_json = r.json()
+json_text = json.dumps(response_json)
 
-elements_df = pd.DataFrame(json['elements'])
-elements_types_df = pd.DataFrame(json['element_types'])
-teams_df = pd.DataFrame(json['teams'])
+write_file("raw-data/stats.json", json_text)
 
-slim_elements_df = elements_df[['second_name','team','element_type','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points']]
+elements_df = pd.DataFrame(response_json['elements'])
+elements_types_df = pd.DataFrame(response_json['element_types'])
+teams_df = pd.DataFrame(response_json['teams'])
+
+slim_elements_df = elements_df[['first_name', 'second_name','team','element_type','minutes','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points','points_per_game']]
 slim_elements_df['team'] = slim_elements_df.team.map(teams_df.set_index('id').name)
 slim_elements_df['value'] = slim_elements_df.value_season.astype(float)
 slim_elements_df['element_type'] = slim_elements_df.element_type.map(elements_types_df.set_index('id').plural_name_short)
 slim_elements_df.rename(index={0: "id"})
 
-goal_df = slim_elements_df.loc[slim_elements_df.element_type == 'GKP']
-def_df = slim_elements_df.loc[slim_elements_df.element_type == 'DEF']
-mid_df = slim_elements_df.loc[slim_elements_df.element_type == 'MID']
-fwd_df = slim_elements_df.loc[slim_elements_df.element_type == 'FWD']
-
-top_5_gk = goal_df.sort_values('value',ascending=False).head(10)
-top_5_def = def_df.sort_values('value',ascending=False).head(10)
-top_5_mid = mid_df.sort_values('value',ascending=False).head(10)
-top_5_fwd = fwd_df.sort_values('value',ascending=False).head(10)
-
-top_5_gk.to_csv('csv/gk.csv')
-top_5_def.to_csv('csv/def.csv')
-top_5_mid.to_csv('csv/mid.csv')
-top_5_fwd.to_csv('csv/fwd.csv')
+positions = {
+  'GKP': 'Goalkeeper', 
+  'DEF': 'Defender', 
+  'MID': 'Midfielder', 
+  'FWD': 'Forward'
+}
 
 html = read_file('templates/page_start.html')
 html += date_line()
 
-html += "<ul class='nav nav-tabs'>"
-html += create_li_item('Goalkeepers', "active")
-html += create_li_item('Defenders', "")
-html += create_li_item('Midfielders', "")
-html += create_li_item('Forwards', "")
-html += "</ul>"
+#upper tabs
+upper_tabs_template = read_file('templates/upper_tabs.html')
+sort_items = ['value', 'total_points', 'points_per_game']
 
-html += "<div class='tab-content'>"
+for sort_item in sort_items:
 
-content = table_maker(top_5_gk, 'Goalkeepers')
-html += create_tab_content('Goalkeepers', content, "active")
+  replacement_tag = f"by_{sort_item}_content_place_holder"
 
-content = table_maker(top_5_def, 'Defenders')
-html += create_tab_content('Defenders', content, "")
+  #lower tabs
+  tabs_html = "<ul class='nav nav-tabs'>"
 
-content = table_maker(top_5_mid, 'Midfielders')
-html += create_tab_content('Midfielders', content, "")
+  for pos_short, pos_long in positions.items():
+    active = "active" if (pos_short == 'GKP') else ""
+    tabs_html += create_li_item(pos_long, active, sort_item)
 
-content = table_maker(top_5_fwd, 'Forwards')
-html += create_tab_content('Forwards', content, "")
+  tabs_html += "</ul>"
+  tabs_html += "<div class='tab-content'>"
 
-html += "</div>"
+  for pos_short, pos_long in positions.items():
+    slim_dataframe = slim_elements_df.loc[slim_elements_df.element_type == pos_short]
+    top5 = slim_dataframe.sort_values(sort_item, ascending=False).head(10)
 
+    content = table_maker(top5, pos_long, sort_item)
+    active = "active" if (pos_short == 'GKP') else ""
+    tabs_html += create_tab_content(pos_long, content, active, sort_item)
+
+  tabs_html += "</div>"
+  upper_tabs_template = upper_tabs_template.replace(replacement_tag, tabs_html)
+
+html += upper_tabs_template
 html += read_file('templates/page_end.html')
 
-f = open("index.php", "w")
-f.write(html)
-f.close
+write_file("index.php", html)
